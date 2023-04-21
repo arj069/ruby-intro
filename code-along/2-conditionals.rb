6632b84df1c8218033f291db1aee7322474c7099
Abishek Raghavan <abishek.raghavan@kellogg.northwestern.edu>
2023-04-21 19:21:26 +0000

    Conditionals lab

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,45 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# this_is_true = true
+# this_is_false = false
+
+# number = 10
 
 # Boolean Expressions
+# puts 3 == 2 outputs false 
+# puts 3 > 2
+# puts number < 15
+# puts number != 51
 
 # If Conditional Logic
+# if 3 == 2
+#     puts "you shouldn't be here"
+# end
 
 # If/Else Conditional Logic
+# user_entered_password = "tacos"
+# real_password = "secret"
+# if user_entered_password == real_password
+#     puts "you're in!"
+# else 
+#     puts "no way!"
+# end
 
 # Elsif Conditional Logic
+# your_score = 2
+# opponent_score = 2
+# if your_score > opponent_score
+#     puts "yay"
+# elsif your_score == opponent_score
+#     puts "ok, you tried"
+# else
+#     puts "sadness"
+# end
 
 # Combining Expressions
+# temp = 70
+# precip = 0
+# if temp >= 66 && temp <= 90 && precip == 0
+#     puts "it's nice!"
+# end
